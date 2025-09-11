a7113b9ea52c26b80b88f5cdb196b97ea0d0881c
Aleah P. <92504607+AleahPorras@users.noreply.github.com>
2025-09-10 19:39:12 -0700

    Updated summarize.py

--- summarize.py
+++ summarize.py
@@ -35,16 +35,21 @@ def get_text(url):
     """Extracts HTML content from the URL to extract the main text."""
     
     try:
+        # sends an http request to the website, header allows for compatibility with Mozilla, timeout prevents request to run longer in the case of an error.
         response = requests.get(url, headers={"User-Agent": "Mozilla/5.0"}, timeout = 10)
-        response.raise_for_status()
+        response.raise_for_status() #checks the HTTPS status code of the request
     
     except Exception:
+        # exits the entire program in an error occurs
         sys.exit(f"An error has occured, try again with a different link.")
     
+    # structures the raw html into a structured tree representation
     soup = BeautifulSoup(response.text, "html.parser")
 
+    # filters out all text in paragraph attributes
     results = [p.get_text() for p in soup.find_all("p")]
     
+    # returns error if no usable text is found
     if not results:
         sys.exit("No text content found at the URL. Try again.")
     
@@ -73,9 +78,9 @@ def summarize_text(url):
         f'Put the provided source URL "{url if url else "N/A"} "into the "references" field.'
         "All this information should be given in true JSON format, url, summary, keywords, and references."
         "If the given URL is invalid, stop the program and print out the error code."
-
     )
     
+    # generates gemini response
     response = client.models.generate_content(
         model = "gemini-1.5-flash",
         config = types.GenerateContentConfig(
@@ -84,22 +89,27 @@ def summarize_text(url):
             top_k = 50,
             max_output_tokens = 512
         ),
+        # actual prompt for the summarizer
         contents = f"{SYSTEM}\n\n[Input]\n{url_text}\n\n[Task]\n{TASK}"
     )
 
+    # removes whitespace
     results = response.text.strip()
 
+    # removes JSON wrapping
     proper_json = results.strip("```json").strip("```")
 
     data = json.loads(proper_json)
     json_output = json.dumps(data, indent=4)
+
+    #writes JSON output into a file
     with open("output.json", "w") as f:
         f.write(json_output)
 
+    # prettified output for terminal
     print(f"\nFrom URL: {data.get('url')}\n")
     print(f"Summary:\n {data.get('summary')}\n")
     print(f"Keywords: {data.get('keywords')}\n")
     print(f"References: {data.get('references')}\n")
 
-# print(get_text(args.url))
 summarize_text(args.url)
